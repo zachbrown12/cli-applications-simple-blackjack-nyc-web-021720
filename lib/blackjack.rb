353c06753b5cc519397ac85a257c1b29039c1b81
Zach Brown <zachmb22@gmail.com>
2020-02-10 03:49:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your card adds up to #{card_total}"
 end
 
 def prompt_user
